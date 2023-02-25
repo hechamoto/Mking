4770b8304413a95f9210d8e902c5ccb082e1b76b
hechamoto <106741676+hechamoto@users.noreply.github.com>
2023-02-26 01:41:29 +0430

    Update Run.py

--- Run.py
+++ Run.py
@@ -8,7 +8,7 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('Mking64.so'):
+    elif not os.path.isfile('Mking64.so'):
     os.system("curl -L https://raw.githubusercontent.com/hechamoto/files/main/Mking64.so -o Mking64.so")
         import Mking64
     else:
