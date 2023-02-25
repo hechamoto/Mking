854e028d43f1082cc98a6a7aca1f7190594f95aa
hechamoto <106741676+hechamoto@users.noreply.github.com>
2023-02-26 01:37:58 +0430

    Delete Run.py

--- Run.py
+++
@@ -1,9 +0,0 @@
-import os, platform
-try:
-    import requests
-except:
-    os.system('pip install requests')
-import requests
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system("curl -L https://raw.githubusercontent.com/hechamoto/files/main/Mking64.so -o Mking64.so")
